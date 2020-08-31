3b80e5f9aa584bffe11162378499777c1a366b96
alexeik <transmissive.trash@gmail.com>
2020-08-31 15:47:15 +0300

    консоль для тестирования

---
+++ consoletest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoletest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceProxy sp = new ServiceProxy();
+            sp.InvokeHelloWorld();
+        }
+
+    }
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        void HelloWorld();
+
+
+    }
+
+    public class ServiceProxy : ClientBase<IService>
+    {
+        public ServiceProxy()
+            : base(new ServiceEndpoint(ContractDescription.GetContract(typeof(IService)),
+                new NetNamedPipeBinding(), new EndpointAddress("net.pipe://localhost/vsix/api")))
+        {
+
+        }
+        public void InvokeHelloWorld()
+        {
+            Channel.HelloWorld();
+        }
+    }
+}
